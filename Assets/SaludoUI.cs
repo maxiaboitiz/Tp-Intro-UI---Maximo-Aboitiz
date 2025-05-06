084e14d52590a69690251d43eab20cdf95c94cfb
maxiaboitiz <166754503+maxiaboitiz@users.noreply.github.com>
2025-05-06 12:13:52 -0300

    Tp 2 Terminado

---
+++ Assets/SaludoUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaludoUI : MonoBehaviour
+{
+    public TextMeshProUGUI saludoUsuario;
+    public TMP_InputField inputNombre;
+    string nombreUsuario;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        saludoUsuario.text = ""; 
+    }
+
+    public void Saludar()
+    {
+        saludoUsuario.text = "Hola " + inputNombre.text;
+    }
+}
